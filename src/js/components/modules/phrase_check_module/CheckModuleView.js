175158e048030a0099afcbaa0a9d01da4d63b1a6
Luke Wilson <lukewilson@Lukes-MacBook-Air.local>
2016-07-08 09:42:10 -0400

    Removed a significant amount of code, essentially breaking the phrase check module to meet flux compliance'

--- src/js/components/modules/phrase_check_module/CheckModuleView.js
+++ src/js/components/modules/phrase_check_module/CheckModuleView.js
@@ -4,10 +4,7 @@ const React = require('react');
 const ScriptureDisplay = require('./subcomponents/ScriptureDisplay');
 const ConfirmDisplay = require('./subcomponents/ConfirmDisplay');
 const FlagDisplay = require('./subcomponents/FlagDisplay');
-const NextButton = require('./subcomponents/NextButton');
 const Menu = require('./subcomponents/Menu');
-const Loader = require('./subcomponents/Loader');
-const Fetcher = require('./FetchData');
 const AbstractCheckModule = require('../../AbstractCheckModule');
 const CheckActions = require('../../../actions/CheckActions.js');
 
@@ -29,36 +26,32 @@ class PhraseChecker extends AbstractCheckModule{
     CheckActions.changeCheckProperty("selectedText", e);
   }
   setFlagState(e){
-    this.setState({flagState: e});
+    CheckActions.changeCheckProperty("checkStatus", e);
   }
-  setNote(e){
-    this.setState({note: e});
-  }
-  setRef(e){
-    this.setState({ref: e});
-  }
-  setProgress(e){
-    this.setState({progress: e});
-    if(this.state.progress >= 100){
-      this.setState({isLoading: false});
-    }
-  }
-  onParserCompletion(object){
-      console.log(object);
-      this.setState({chapterData: object});
-  }
+  // setNote(e){
+  //   this.setState({note: e});
+  // }
+  // setRef(e){
+  //   this.setState({ref: e});
+  // }
+  // setProgress(e){
+  //   this.setState({progress: e});
+  //   if(this.state.progress >= 100){
+  //     this.setState({isLoading: false});
+  //   }
+  // }
+  // onParserCompletion(object){
+  //     console.log(object);
+  //     this.setState({chapterData: object});
+  // }
   componentWillMount(){
     this.setState({
       toCheck: "",
       ref: "",
       note: "",
-      chapterData: {},
       tlVerse: "This is selectable placeholder text. Eventually the target language text will appear here",
       selectedText: "",
       flagState: "",
-      returnObject: [],
-      progress:  0,
-      isLoading: true
     });
     var data = Fetcher('eph', this.setProgress, this.onParserCompletion);
   }
@@ -72,23 +65,10 @@ class PhraseChecker extends AbstractCheckModule{
         note: this.state.note
       }
     );
-    this.setState({returnObject: object});
-    console.log(this.state.returnObject);
   }
   render() {
     return (
       <Grid>
-        <Row
-          className="progressBar"
-          style={{
-            display: this.state.isLoading ? "block" : "none"
-          }}
-        >
-          <Col md={12}>
-            <Loader progress={this.state.progress} />
-          </Col>
-        </Row>
-        <div style={{display: this.state.isLoading ? "none" : "block"}}>
           <Row className="show-grid">
             <Col md={12}>
               <ScriptureDisplay
@@ -114,18 +94,12 @@ class PhraseChecker extends AbstractCheckModule{
             </Col>
           </Row>
           <Row>
-            <Col md={12} className="next-button">
-              <NextButton nextItem={this.appendReturnObject}/>
-            </Col>
-          </Row>
-          <Row>
             <Col md={12}>
               <Menu verses={this.state.chapterData}
                         setNote={this.setNote}
                         setRef={this.setRef}/>
             </Col>
           </Row>
-        </div>
       </Grid>
     );
   }
